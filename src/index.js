b28f1ff0a87048db555d47e554359d0c455fe005
Guillermo Santos <guillermo.santosba@gmail.com>
2022-05-03 17:10:23 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';  // React Library
+import ReactDOM from 'react-dom';   // DOM library
+import './index.scss';  // Import CSS
+import App from './components/App.jsx';
+
+ReactDOM.render(
+    <App />
+    , document.getElementById('app'));   // Render the react app
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanPlugin } = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'index_bundle-[hash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // Use babel with webpack
+                },
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // Use babel with webpack
+                },
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CleanPlugin(),
+    ],
+};
