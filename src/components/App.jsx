19fe945f2a8fda982292b22dfabf24aa5adcc099
Guillermo Santos <guillermo.santosba@gmail.com>
2022-05-04 12:26:41 -0600

    Showing the number when the calculator gets pressed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { arrayToNumber } from '../utils/operations';
 
 function App() {
+  const [display, setDisplay] = useState([])
+  const [ans, setAns] = useState(0);
+  const [currentOperation, setCurrentOperation] = useState(null); 
+
+  /**
+   * Event indicating that a number was pressed on the UI
+   * @param {number} value 
+   */
+  const numberPressed = (value) => {
+    if (display.length === 9)  return;  // Only 9 slots available
+    if (value === '.' && display.find(i => i === '.')) return;  // Cannot have two dots on the same expression
+    if (value === '.' && display.length === 0) {
+      setDisplay([0,'.'])
+      return
+    }
+    setDisplay([...display, value]);
+  }
+
+  /**
+   * Event that runs when the clear button was pressed
+   */
+  const clearPressed = () => {
+    setAns(0);
+    setDisplay([]);
+  };
+
+  /**
+   * Event that runs when an operator was pressed
+   * @param {string} operator
+   */
+  const operatorPressed = (operator) => {
+    setCurrentOperation(operator);    
+  }
+  
+
+
   return (
     <div className='calculator'>
       <div className="calculator__screen">
-        <span className='calculator__cell'>3</span>
-        <span className='calculator__cell'>.</span>
-        <span className='calculator__cell'>1</span>
-        <span className='calculator__cell'>4</span>
-        <span className='calculator__cell'>4</span>
-        <span className='calculator__cell'>4</span>
-        <span className='calculator__cell'>4</span>
+        {display.map(item => <span key={item-`${Math.random()}`} className='calculator__cell'>{item}</span> )}
+    
       </div>
       <div className="calculator__keyboard">
-        <button className='calculator__button calculator__button--c'>C</button>
-        <button className='calculator__button calculator__button--rev'>+/-</button>
-        <button className='calculator__button calculator__button--mod'>%</button>
-        <button className='calculator__button calculator__button--div'>/</button>
-        <button className='calculator__button calculator__button--mul'>X</button>
-        <button className='calculator__button calculator__button--min'>-</button>
-        <button className='calculator__button calculator__button--plus'>+</button>
-        <button className='calculator__button calculator__button--dot'>.</button>
-        <button className='calculator__button calculator__button--eq'>=</button>
-        <button className='calculator__button calculator__button--1'>1</button>
-        <button className='calculator__button calculator__button--2'>2</button>
-        <button className='calculator__button calculator__button--3'>3</button>
-        <button className='calculator__button calculator__button--4'>4</button>
-        <button className='calculator__button calculator__button--5'>5</button>
-        <button className='calculator__button calculator__button--6'>6</button>
-        <button className='calculator__button calculator__button--7'>7</button>
-        <button className='calculator__button calculator__button--8'>8</button>
-        <button className='calculator__button calculator__button--9'>9</button>
-        <button className='calculator__button calculator__button--0'>0</button>
+        <button className='calculator__button calculator__button--c' onClick={clearPressed}>C</button>
+        <button className='calculator__button calculator__button--rev' onClick={() => {operatorPressed('+/-')}}>+/-</button>
+        <button className='calculator__button calculator__button--mod' onClick={() => {operatorPressed('%')}}>%</button>
+        <button className='calculator__button calculator__button--div' onClick={() => {operatorPressed('/')}}>/</button>
+        <button className='calculator__button calculator__button--mul' onClick={() => {operatorPressed('x')}}>X</button>
+        <button className='calculator__button calculator__button--min' onClick={() => {operatorPressed('-')}}>-</button>
+        <button className='calculator__button calculator__button--plus' onClick={() => {operatorPressed('+')}}>+</button>
+        <button className='calculator__button calculator__button--dot' onClick={()=>{numberPressed('.')}}>.</button>
+        <button className='calculator__button calculator__button--eq' onClick={() => {operatorPressed('=')}}>=</button>
+        <button className='calculator__button calculator__button--1' 
+        onClick={() => {numberPressed(1)}}>
+          1
+          </button>
+        <button className='calculator__button calculator__button--2' 
+        onClick={() => {numberPressed(2)}}>
+          2
+          </button>
+        <button className='calculator__button calculator__button--3' 
+        onClick={() => {numberPressed(3)}}>
+          3
+          </button>
+        <button className='calculator__button calculator__button--4' 
+        onClick={() => {numberPressed(4)}}>
+          4
+          </button>
+        <button className='calculator__button calculator__button--5' 
+        onClick={() => {numberPressed(5)}}>
+          5
+          </button>
+        <button className='calculator__button calculator__button--6' 
+        onClick={() => {numberPressed(6)}}>
+          6
+          </button>
+        <button className='calculator__button calculator__button--7' 
+        onClick={() => {numberPressed(7)}}>
+          7
+          </button>
+        <button className='calculator__button calculator__button--8' 
+        onClick={() => {numberPressed(8)}}>
+          8
+          </button>
+        <button className='calculator__button calculator__button--9' 
+        onClick={() => {numberPressed(9)}}>
+          9
+          </button>
+        <button className='calculator__button calculator__button--0' 
+        onClick={() => {numberPressed(0)}}>
+          0
+          </button>
       </div>
     </div>
   )
---
+++ src/utils/operations.js
@@ -0,0 +1,46 @@
+
+
+/**
+ * Adds two numbers
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+export const add = (a, b) => a + b;
+
+
+/**
+ * Divide
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+export const divide = (a, b) => a / b;
+
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+export const multiply = (a, b) => a * b;
+
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+export const mod = (a, b) => a % b;
+
+
+/**
+ * Converts an array to a number
+ * @param {number[]} arr 
+ * @returns 
+ */
+export const arrayToNumber = (arr) => {
+    return Number(arr.join(''))
+}
