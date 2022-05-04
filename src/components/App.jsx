5e40a86fc499cd855f07af523cc4ed382902fc3c
Guillermo Santos <guillermo.santosba@gmail.com>
2022-05-04 11:00:49 -0600

    Implemented grid ordering on items.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,25 @@ function App() {
         <span className='calculator__cell'>4</span>
       </div>
       <div className="calculator__keyboard">
-        
+        <button className='calculator__button calculator__button--c'>C</button>
+        <button className='calculator__button calculator__button--rev'>+/-</button>
+        <button className='calculator__button calculator__button--mod'>%</button>
+        <button className='calculator__button calculator__button--div'>/</button>
+        <button className='calculator__button calculator__button--mul'>X</button>
+        <button className='calculator__button calculator__button--min'>-</button>
+        <button className='calculator__button calculator__button--plus'>+</button>
+        <button className='calculator__button calculator__button--dot'>.</button>
+        <button className='calculator__button calculator__button--eq'>=</button>
+        <button className='calculator__button calculator__button--1'>1</button>
+        <button className='calculator__button calculator__button--2'>2</button>
+        <button className='calculator__button calculator__button--3'>3</button>
+        <button className='calculator__button calculator__button--4'>4</button>
+        <button className='calculator__button calculator__button--5'>5</button>
+        <button className='calculator__button calculator__button--6'>6</button>
+        <button className='calculator__button calculator__button--7'>7</button>
+        <button className='calculator__button calculator__button--8'>8</button>
+        <button className='calculator__button calculator__button--9'>9</button>
+        <button className='calculator__button calculator__button--0'>0</button>
       </div>
     </div>
   )
